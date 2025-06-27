b224a983f5189c64c4603927052b057b934ddab2
eshwarbkkkk <148619149+eshwarbkkkk@users.noreply.github.com>
2025-06-27 11:43:14 +0530

    Initial commit - Spring Boot setup

---
+++ community-alert-system/src/main/java/com/safezone/community_alert_system/CommunityAlertSystemApplication.java
@@ -0,0 +1,13 @@
+package com.safezone.community_alert_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityAlertSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityAlertSystemApplication.class, args);
+	}
+
+}
---
+++ community-alert-system/src/test/java/com/safezone/community_alert_system/CommunityAlertSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safezone.community_alert_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityAlertSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
